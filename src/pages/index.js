fe403f37808b73a91db92efa3f766d46242e50aa
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-07-28 04:06:03 +0600

    Product Card Component is Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,27 @@
 import RootLayout from "@/components/Layouts/RootLayout"
+import ProductCard from "@/components/UI/ProductCard"
+import Head from "next/head"
 
 export default function HomePage() {
 
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <main>
+        <div>
+          <h2 className="text-2xl text-center">Featured Products</h2>
+          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-8 my-10">
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+            <ProductCard></ProductCard>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
