783810f6fd9f06749f349e5c7d5196840d3fe85b
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2023-07-28 01:09:48 +0600

    Tested Fake data and everything's right

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,19 @@
 import RootLayout from "@/components/Layouts/RootLayout"
+import Image from "next/image"
 
-export default function HomePage() {
+export default function HomePage({ products }) {
+
+  console.log(products)
+
   return (
-    <h1>Home Page</h1>
+    <div>
+      {
+        products.map(product => <div key={product.name}>
+          <h1>{product.name}</h1>
+          <Image src={product.image} width={200} height={200}  alt=""/>
+        </div>)
+      }
+    </div>
   )
 }
 
@@ -10,4 +21,16 @@ HomePage.getLayout = function getLayout(page) {
   return (
     <RootLayout>{page}</RootLayout>
   )
+}
+
+export const getServerSideProps = async () => {
+
+  const res = await fetch('http://localhost:5000/products')
+  const products = await res.json()
+
+  return {
+    props: {
+      products
+    }
+  }
 }
